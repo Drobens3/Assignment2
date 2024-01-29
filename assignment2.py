6c8423669d34d062c227c83c3a55ee18b6b02bc9
Robens Duverny <103876887+Drobens3@users.noreply.github.com>
2024-01-28 21:16:58 -0500

    full code

---
+++ assignment2.py
@@ -0,0 +1,47 @@
+import socket;
+class Assignment2:
+
+    #Task 1 (Constructor)
+    def __init__(self, year):
+        self.year = year
+
+    #Task 2 (Age)
+    def tellAge(self, currentYear):
+        print("Your age is %d" % (currentYear - self.year))
+
+    #Task 3 (List)
+    def listAnniversaries(self):
+        currentYear = 2022
+        anniversaries = [(year - self.year) for year in range(self.year + 10, currentYear, 10)]
+        return anniversaries
+
+    #Task 4 (String Manipulation)
+    def modifyYear(self, n):
+        year_str = str(self.year)
+        modified_year = year_str[:2] * n
+        for i in range(1, len(year_str), 2):
+            modified_year += year_str[i] * n
+        return modified_year
+    
+    #Task 5 (Loop and Conditional statements)
+    def checkGoodString(string):
+        if len(string) < 9:
+            return False
+        if not string[0].islower():
+            return False
+        digit_count = sum(1 for char in string if char.isdigit())
+        if digit_count != 1:
+            return False
+        return True
+    
+    #Task 6 (Socket)
+    def connectTcp(host, port):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, port))
+            s.close()
+            return True
+        except Exception as e:
+            print("Error:", e)
+            return False
+    
